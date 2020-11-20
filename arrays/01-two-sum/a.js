896aba06800a83c8c6f5ad7142a012b621f8f4f4
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-11-20 15:40:57 +0700

    Add pretter format

--- arrays/01-two-sum/a.js
+++ arrays/01-two-sum/a.js
@@ -1,17 +1,18 @@
-
-console.log(`Indices of two numbers is: `, indicesOfTwoNumbersAddUpToTarget([3,3], 6));
+console.log(
+    `Indices of two numbers is: `,
+    indicesOfTwoNumbersAddUpToTarget([3, 3], 6)
+)
 
 function indicesOfTwoNumbersAddUpToTarget(numbers, target) {
-    const numbersMap  =  {};
-    for(let i = 0; i< numbers.length; i++) {
-        const numberToFind = target - numbers[i];
-        if(numbersMap[numberToFind] !== undefined) {
-            return [numbersMap[numberToFind], i];
+    const numbersMap = {}
+    for (let i = 0; i < numbers.length; i++) {
+        const numberToFind = target - numbers[i]
+        if (numbersMap[numberToFind] !== undefined) {
+            return [numbersMap[numberToFind], i]
         } else {
-            numbersMap[numbers[i]] = i;
+            numbersMap[numbers[i]] = i
         }
     }
 
-    return null;
-
+    return null
 }
--- arrays/02-most-water-container/a.js
+++ arrays/02-most-water-container/a.js
@@ -1,22 +1,23 @@
-
-console.log(`Most water container is: `, calcMostWaterContainer([1,8,6,2,5,4,8,3,7]));
+console.log(
+    `Most water container is: `,
+    calcMostWaterContainer([1, 8, 6, 2, 5, 4, 8, 3, 7])
+)
 
 function calcMostWaterContainer(heights) {
-    let maxArea = 0;
-    let i = 0;
-    let j = heights.length - 1;
-    while(i<j) {
-        const height = Math.min(heights[i], heights[j]);
-        const width = j - i;
-        const newMax = height*width;
-        maxArea = Math.max(newMax, maxArea);
-        if(heights[i] < heights[j]) {
-            i++;
+    let maxArea = 0
+    let i = 0
+    let j = heights.length - 1
+    while (i < j) {
+        const height = Math.min(heights[i], heights[j])
+        const width = j - i
+        const newMax = height * width
+        maxArea = Math.max(newMax, maxArea)
+        if (heights[i] < heights[j]) {
+            i++
         } else {
-            j--;
+            j--
         }
     }
 
-    return maxArea;
-
+    return maxArea
 }
