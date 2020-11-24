400c7acf2d40a9bd34be9f066fcf194cadf5739f
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-11-24 14:59:48 +0700

    Add normal solution for most water container question

--- arrays/02-most-water-container/a.js
+++ arrays/02-most-water-container/a.js
@@ -1,23 +1,37 @@
 console.log(
     `Most water container is: `,
     calcMostWaterContainer([1, 8, 6, 2, 5, 4, 8, 3, 7])
-)
+);
 
 function calcMostWaterContainer(heights) {
-    let maxArea = 0
-    let i = 0
-    let j = heights.length - 1
+    let maxArea = 0;
+    let i = 0;
+    let j = heights.length - 1;
     while (i < j) {
-        const height = Math.min(heights[i], heights[j])
-        const width = j - i
-        const newMax = height * width
-        maxArea = Math.max(newMax, maxArea)
+        const height = Math.min(heights[i], heights[j]);
+        const width = j - i;
+        const newMax = height * width;
+        maxArea = Math.max(newMax, maxArea);
         if (heights[i] < heights[j]) {
-            i++
+            i++;
         } else {
-            j--
+            j--;
         }
     }
 
-    return maxArea
+    return maxArea;
 }
+
+// function calcMostWaterContainer(heights) {
+//     let maxArea = 0;
+//     for (let i = 0; i < heights.length; i++) {
+//         for (let j = i + 1; j < heights.length; j++) {
+//             const height = Math.min(heights[i], heights[j]);
+//             const width = j - i;
+//             const newMax = height * width;
+//             maxArea = Math.max(newMax, maxArea);
+//         }
+//     }
+
+//     return maxArea;
+// }
