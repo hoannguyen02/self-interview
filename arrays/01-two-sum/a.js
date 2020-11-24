87c2d54a63c8a9cae3fa91ee5beaea7142d4acdc
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-11-24 14:11:15 +0700

    Add normal solution for two sum question

--- arrays/01-two-sum/a.js
+++ arrays/01-two-sum/a.js
@@ -1,18 +1,31 @@
 console.log(
     `Indices of two numbers is: `,
-    indicesOfTwoNumbersAddUpToTarget([3, 3], 6)
-)
+    indicesOfTwoNumbersAddUpToTarget([2, 7, 11, 15], 9)
+);
 
 function indicesOfTwoNumbersAddUpToTarget(numbers, target) {
-    const numbersMap = {}
+    const numbersMap = {};
     for (let i = 0; i < numbers.length; i++) {
-        const numberToFind = target - numbers[i]
+        const numberToFind = target - numbers[i];
         if (numbersMap[numberToFind] !== undefined) {
-            return [numbersMap[numberToFind], i]
+            return [numbersMap[numberToFind], i];
         } else {
-            numbersMap[numbers[i]] = i
+            numbersMap[numbers[i]] = i;
         }
     }
 
-    return null
+    return null;
 }
+
+// function indicesOfTwoNumbersAddUpToTarget(numbers, target) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         const numberToFind = target - numbers[i];
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             if (numbers[j] === numberToFind) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+
+//     return [];
+// }
